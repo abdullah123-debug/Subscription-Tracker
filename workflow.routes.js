235a39be11bb50969380c4e7415c9b2f700c673f
abdullah123-debug <234abdullahbutt2@gmail.com>
2025-11-19 09:26:03 -0800

    Delete workflow.routes.js

--- workflow.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendReminders } from "../controllers/workflow.controller.js";
-
-const workflowRouter = express.Router();
-
-workflowRouter.post("/subscription/reminder", sendReminders);
-
-export default workflowRouter;
